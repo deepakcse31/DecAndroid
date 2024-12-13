bc69833d1c22e162c67d5994b39ecc790ed3484a
deepak <deepakcse31@gmail.com>
2024-12-13 21:49:56 +0530

    Seesion management

--- app/src/main/java/com/example/decandroid/LoginActivity.java
+++ app/src/main/java/com/example/decandroid/LoginActivity.java
@@ -1,6 +1,13 @@
 package com.example.decandroid;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +16,50 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
-
+    EditText edtmobileno,edtpassword;
+    Button btnlogin;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+        edtmobileno=findViewById(R.id.edtmobileno);
+        edtpassword=findViewById(R.id.edtpassword);
+        btnlogin=findViewById(R.id.btnsubmit);
+
+        SharedPreferences sharedPreferences=getSharedPreferences("onx",MODE_PRIVATE);
+        SharedPreferences.Editor myedit=sharedPreferences.edit();
+
+
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mobileno=edtmobileno.getText().toString();
+                String password=edtpassword.getText().toString();
+
+                if (mobileno.equals("9876543210") && password.equals("123456"))
+                {
+
+                    myedit.putString("loginstatus","true");
+                    myedit.apply();
+                    finish();
+                    Toast.makeText(getApplicationContext(),"Login Successfully",Toast.LENGTH_LONG).show();
+
+                    Intent intent=new Intent(getApplicationContext(),HomeActivity.class);
+                    startActivity(intent);
+                }
+                else {
+
+                Toast.makeText(getApplicationContext(),"Please Enter Correct Credentials",Toast.LENGTH_LONG).show();
+                }
+
+                //Toast.makeText(getApplicationContext(),mobileno+" "+password,Toast.LENGTH_LONG).show();
+            }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
---
+++ app/src/main/java/com/example/decandroid/SplashActivity.java
@@ -0,0 +1,51 @@
+package com.example.decandroid;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Locale;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SharedPreferences sharedPreferences=getSharedPreferences("onx",MODE_PRIVATE);
+                String loginstatus = sharedPreferences.getString("loginstatus", "");
+                if (loginstatus.equals("true")) {
+                    Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                }
+                else {
+
+                Intent intent=new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(intent);
+                }
+
+//                Intent intent=new Intent(SplashActivity.this,LoginActivity.class);
+//                startActivity(intent);
+//
+            }
+        },2000);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
